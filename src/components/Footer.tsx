90875d2bf034d2615420016049da2371a50b4ef2
Maksym Kondratov <geset300@gmail.com>
2024-08-22 16:13:33 +0200

    Reset

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.container}>KUBIKI 2024</div>;
+  return <div className={styles.container}>KUBIKI 2024!</div>;
 };
 
 export default Footer;
