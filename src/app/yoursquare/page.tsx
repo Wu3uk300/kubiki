3816a12179a03ee965fc1563e964df4e7f757b9b
Maksym Kondratov <geset300@gmail.com>
2024-08-28 12:50:22 +0200

    still fixing bug

--- src/app/yoursquare/page.tsx
+++ src/app/yoursquare/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/components/NavBar";
 import styles from "@/styles/YourSquares.module.css";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
 
 import { redirect } from "next/navigation";
 import Square from "@/components/Square";
