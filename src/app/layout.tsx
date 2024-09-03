647afd9671f7c2fd1578c0a22b3979356c89ff44
Maksym Kondratov <geset300@gmail.com>
2024-09-03 17:05:31 +0200

    rn without logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import logo from "@/images/Kubiki.jpg";
+
 import { Figtree } from "next/font/google";
 import "../styles/normalize.css";
 const inter = Figtree({ subsets: ["latin"], weight: "400" });
@@ -16,9 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="@/images/Kubiki.jpg" />
-      </head>
       <body className={inter.className}>
         <main>{children}</main>
       </body>
